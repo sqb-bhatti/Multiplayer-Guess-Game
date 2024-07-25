9c17e2f6f3e523c7bc7a88002aed41183c1ef36c
saqibbhatti <sqb_bhatti@yahoo.com>
2024-07-25 17:30:17 +0800

    Completed and updated the design

---
+++ GuessPersonality/DrawingView.swift
@@ -0,0 +1,18 @@
+//
+//  DrawingView.swift
+//  GuessPersonality
+//
+//  Created by Saqib Bhatti on 25/7/24.
+//
+
+import SwiftUI
+
+struct DrawingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawingView()
+}
