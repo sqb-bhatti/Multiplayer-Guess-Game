4cff36042c2196e279e6301627be01b36127aa66
saqibbhatti <sqb_bhatti@yahoo.com>
2024-07-25 17:32:43 +0800

    Completed and updated the design

--- GuessPersonality/DrawingView.swift
+++ GuessPersonality/DrawingView.swift
@@ -2,17 +2,53 @@
 //  DrawingView.swift
 //  GuessPersonality
 //
-//  Created by Saqib Bhatti on 25/7/24.
-//
+
 
 import SwiftUI
+import PencilKit
 
-struct DrawingView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+struct DrawingView: UIViewRepresentable {
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var matchManager: MatchManager
+        
+        init(matchManager: MatchManager) {
+            self.matchManager = matchManager
+        }
+        
+        func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+            //TODO: Send the new drawing data
+        }
+    }
+    
+    
+    var canvasView = PKCanvasView()
+    
+    @ObservedObject var matchManager: MatchManager
+    @Binding var eraserEnabled: Bool
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.drawingPolicy = .anyInput
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 5.0)
+        canvasView.delegate = context.coordinator
+        canvasView.isUserInteractionEnabled = matchManager.currentlyDrawing
+        
+        return canvasView
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(matchManager: matchManager)
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        //TODO: Handle various updates
+        
+        canvasView.tool = eraserEnabled ? PKEraserTool(.vector) : PKInkingTool(.pen, color: .black, width: 5.0)
     }
 }
 
-#Preview {
-    DrawingView()
-}
+//#Preview {
+//    @State var eraser = false
+//    
+//    DrawingView(matchManager: MatchManager(), eraserEnabled: $eraser)
+//}
--- GuessPersonality/GameView.swift
+++ GuessPersonality/GameView.swift
@@ -2,17 +2,170 @@
 //  GameView.swift
 //  GuessPersonality
 //
-//  Created by Saqib Bhatti on 25/7/24.
-//
+
 
 import SwiftUI
 
 struct GameView: View {
     @ObservedObject var matchManager: MatchManager
     @State var drawingGuess = ""
+    @State var eraserEnabled = false
+    
+    func makeGuess() {
+        // TODO: Submit the guess
+    }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            GeometryReader {_ in
+                Image(matchManager.currentlyDrawing ? "drawBG": "guesserBG")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .scaleEffect(1.1)
+                
+                VStack {
+                    topBar
+                    
+                    ZStack {
+                        DrawingView(matchManager: matchManager, eraserEnabled: $eraserEnabled)
+                            .aspectRatio(1, contentMode: .fit)
+                            .overlay(RoundedRectangle(cornerRadius: 10.0)
+                                .stroke(.black, lineWidth: 10))
+                        
+                        VStack {
+                            HStack {
+                                Spacer()
+                                if matchManager.currentlyDrawing {
+                                    Button {
+                                        eraserEnabled.toggle()
+                                    } label: {
+                                        Image(systemName: eraserEnabled ? "eraser.fill" : "eraser")
+                                            .font(.title)
+                                            .foregroundStyle(Color("primaryPurple"))
+                                            .padding(.top, 10)
+                                    }
+                                }
+                            }
+                            
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                    pastGuesses
+                }
+                .padding(.horizontal, 30)
+                .ignoresSafeArea(.keyboard, edges: .bottom)
+            }
+            
+            VStack {
+                Spacer()
+                promptGroup
+            }
+            .ignoresSafeArea(.container)
+        }
+    }
+    
+    var topBar: some View {
+        ZStack {
+            HStack {
+                Button {
+                    //TODO: Disconnect from game
+                } label: {
+                    Image(systemName: "arrowshape.turn.up.left.circle.fill")
+                        .font(.largeTitle)
+                        .tint(Color(matchManager.currentlyDrawing ? "primaryPurple": "primaryYellow"))
+                }
+                Spacer()
+                Label("\(matchManager.remainingTime)", systemImage: "clock.fill")
+                    .bold()
+                    .font(.title2)
+                    .foregroundColor(Color(matchManager.currentlyDrawing ? "primaryPurple": "primaryYellow"))
+            }
+        }
+        .padding(.vertical, 15)
+    }
+    
+    
+    var pastGuesses: some View {
+        ScrollView {
+            ForEach(matchManager.pastGuesses) { guess in
+                HStack {
+                    Text(guess.message)
+                        .font(.title2)
+                        .bold(guess.correct)
+                    
+                    if guess.correct {
+                        Image(systemName: "hand.thumbsup.fill")
+                            .foregroundStyle(matchManager.currentlyDrawing ? Color(red: 0.808, green: 0.345, blue: 0.776) :
+                                Color(red: 0.243, green: 0.773, blue: 0.745))
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, 1)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background((matchManager.currentlyDrawing ?
+                    Color(red: 0.243, green: 0.773, blue: 0.745) :
+                    Color("primaryYellow")
+        )
+        .brightness(-0.2)
+        .opacity(0.5)
+        )
+        .cornerRadius(20)
+        .padding(.vertical)
+        .padding(.bottom, 130)
+    }
+    
+    var promptGroup: some View {
+        VStack {
+            if matchManager.currentlyDrawing {
+                Label("DRAW: ", systemImage: "exclamationmark.bubble.fill")
+                    .font(.title2)
+                    .bold()
+                    .foregroundStyle(Color(.white))
+                Text(matchManager.drawPrompt.uppercased())
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                    .foregroundStyle(Color("primaryYellow"))
+            } else {
+                HStack {
+                    Label("GUESS THE DRAWING!: ", systemImage: "exclamationmark.bubble.fill")
+                        .font(.title2)
+                        .bold()
+                        .foregroundStyle(Color("primaryPurple"))
+                    
+                    Spacer()
+                }
+                
+                HStack {
+                    TextField("Type your guess", text: $drawingGuess)
+                        .padding()
+                        .background(
+                            Capsule(style: .circular)
+                                .fill(.white))
+                    Button {
+                        makeGuess()
+                    } label: {
+                        Image(systemName: "chevron.right.circle.fill")
+                            .renderingMode(.original)
+                            .foregroundStyle(Color("primaryPurple"))
+                            .font(.system(size: 50))
+                }
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding([.horizontal, .bottom], 30)
+        .padding(.vertical)
+        .background(
+            matchManager.currentlyDrawing ? 
+            Color(red: 0.243, green: 0.773, blue: 0.745) :
+            Color("primaryYellow"))
+        .opacity(0.5)
     }
 }
 
